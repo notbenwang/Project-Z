a39f39292b346f876f92c1412e9e464324ad413a
notbenwang <112118953+notbenwang@users.noreply.github.com>
2023-11-29 01:42:57 -0500

    Add project files.

---
+++ Pong/Game1.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Reflection.PortableExecutable;
+
+namespace Pong
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Character player;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+        enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+            UpLeft,
+            UpRight,
+            DownLeft,
+            DownRight
+        }
+        class Character
+        {
+            public Texture2D texture;
+            public Vector2 position;
+            public float velocity;
+            public Direction direction;
+
+            public Character(Texture2D texture, Vector2 position, float velocity, Direction direction)
+            {
+                this.texture = texture;
+                this.position = position;
+                this.velocity = velocity;
+                this.direction = direction;
+            }
+        }
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 1080;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+            
+            Vector2 position = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+            float speed = 400f;
+            player = new Character(null, position, speed, Direction.Left);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            player.texture = Content.Load<Texture2D>("player");
+        }
+        private float GetAngleFromDirection(Direction direction) 
+        {
+            switch (direction)
+            {
+                case Direction.Right:
+                    return 0f;
+
+                case Direction.Left:
+                    return (float) Math.PI;
+
+                case Direction.Down:
+                    return (float)Math.PI / 2;
+
+                case Direction.Up:
+                    return (float)Math.PI * 3 / 2;
+
+                case Direction.UpLeft:
+                    return (float)Math.PI * 5 / 4;
+
+                case Direction.UpRight:
+                    return (float)Math.PI * 7 / 4;
+
+                case Direction.DownLeft:
+                    return (float)Math.PI * 3 / 4;
+                
+                case Direction.DownRight:
+                    return (float)Math.PI / 4;
+            }
+            return 0f;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            var kstate = Keyboard.GetState();
+
+            // Event Readers
+            if (kstate.IsKeyDown(Keys.A))
+            {
+                player.position.X -= player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                player.direction = Direction.Left;
+            }
+
+            if (kstate.IsKeyDown(Keys.D))
+            {
+                player.position.X += player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                player.direction = Direction.Right;
+            }
+
+            if (kstate.IsKeyDown(Keys.W))
+            {
+                player.position.Y -= player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (kstate.IsKeyDown(Keys.A))
+                {
+                    player.direction = Direction.UpLeft;
+                }
+                else if (kstate.IsKeyDown(Keys.D))
+                {
+                    player.direction = Direction.UpRight;
+                }
+                else player.direction = Direction.Up;
+            }
+
+            if (kstate.IsKeyDown(Keys.S))
+            {
+                player.position.Y += player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (kstate.IsKeyDown(Keys.A))
+                {
+                    player.direction = Direction.DownLeft;
+                }
+                else if (kstate.IsKeyDown(Keys.D))
+                {
+                    player.direction = Direction.DownRight;
+                }
+                else player.direction = Direction.Down;
+            }
+
+
+            // Player Bound Managing
+            if (player.position.X < player.texture.Width / 2)
+            {
+                player.position.X = player.texture.Width / 2;
+            }
+            else if (player.position.X > _graphics.PreferredBackBufferWidth - player.texture.Width / 2)
+            {
+                player.position.X = _graphics.PreferredBackBufferWidth - player.texture.Width / 2;
+            }
+
+            if (player.position.Y < player.texture.Width / 2)
+            {
+                player.position.Y = player.texture.Width / 2;
+            }
+            else if (player.position.Y > _graphics.PreferredBackBufferHeight - player.texture.Height / 2)
+            {
+                player.position.Y = _graphics.PreferredBackBufferHeight - player.texture.Height / 2;
+            }
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            
+            _spriteBatch.Begin();
+            // Player Sprite
+            _spriteBatch.Draw(
+                player.texture, 
+                player.position, 
+                null, 
+                Color.White, 
+                GetAngleFromDirection(player.direction), 
+                new Vector2(player.texture.Width / 2, player.texture.Height / 2), 
+                new Vector2(0.15f, 0.15f),
+                SpriteEffects.None, 
+                0f);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
