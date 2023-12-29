95033a9559bd0a70818a395068202bc60db6ba4c
notbenwang <112118953+notbenwang@users.noreply.github.com>
2023-12-29 18:45:09 -0500

    collision + resolution implemented

--- Pong/Game1.cs
+++ Pong/Game1.cs
@@ -14,7 +14,8 @@ namespace Pong
         private SpriteBatch _spriteBatch;
         private List<Character> bulletList;
         private List<Character> enemyList;
-        private int bulletCoolDown = 20;
+        private int bulletTimer = 0;
+        private int _bulletCD = 50;
         Character player;
 
         public Game1()
@@ -49,6 +50,45 @@ namespace Pong
                 this.direction = direction;
                 this.health = health;
             }
+            
+            public float left()  {return position.X - (texture.Width / 2);}
+            public float right() { return position.X + (texture.Width / 2);}
+            public float top() { return position.Y - (texture.Height / 2);}
+            public float bottom() { return position.Y + (texture.Height / 2);}
+            public bool Touches(Character other)
+            {
+                // FROM GAMEBOX
+                float l = other.left() - right();
+                float r = left() - other.right();
+                float t = other.top() - bottom();
+                float b = top() - other.bottom();
+                return Enumerable.Max(new [] {l,b,r,t}) <= 0;
+            }
+            public float[] Overlap(Character other)
+            {
+                // FROM GAMEBOX
+                float l = other.left() - right();
+                float r = left() - other.right();
+                float t = other.top() - bottom();
+                float b = top() - other.bottom();
+                float m = Enumerable.Max(new[] { l, b, r, t });
+                if (m >= 0) { return new float[] {0,0}; } 
+                else if (m == l) { return new float[] { l, 0 }; }
+                else if (m == r) { return new float[] { -1 * r, 0 }; }
+                else if (m == t) { return new float[] { 0, t }; }
+                else { return new float[] { 0, -1 * b }; }
+            }
+            public void moveBothToStopOverlap(Character other)
+            {
+                float[] o = Overlap(other);
+                if (o[0] != 0 && o[1] != 0) // o != [0,0]
+                {
+                    position.X = o[0]/2;
+                    position.Y = o[1]/2;
+                    other.position.X = -1 * o[0]/2;
+                    other.position.Y = -1 * o[1]/2;
+                }
+            }
         }
         
         protected override void Initialize()
@@ -61,6 +101,9 @@ namespace Pong
             Vector2 position = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
             float speed = 300f;
             player = new Character(null, position, speed, Direction.Left, 5);
+            
+            for (int i = 0; i < 10; i++) MakeEnemy();
+            
             base.Initialize();
         }
 
@@ -106,7 +149,7 @@ namespace Pong
             float bullet_speed = 10f;
             Texture2D whiteRect = new Texture2D(GraphicsDevice, 1, 1);
             whiteRect.SetData(new[] { Color.White });
-            Character bullet = new Character(whiteRect, position, bullet_speed, direction, 0);
+            Character bullet = new Character(whiteRect, position, bullet_speed, direction, 1);
             bulletList.Add(bullet);
         }
         private void MakeEnemy()
@@ -118,7 +161,7 @@ namespace Pong
             Texture2D redRect = new Texture2D(GraphicsDevice, 1, 1);
             redRect.SetData(new[] { Color.Red });
             Vector2 position = new Vector2(0,0);
-            float enemy_speed = 1.5f;
+            float enemy_speed = 0.5f;
             switch (side)
             {
                 case 1:
@@ -154,17 +197,15 @@ namespace Pong
                     if (player.position.X < enemy.position.X) theta = (float)Math.PI * 3;
                     else theta = (float)Math.PI * 2;
                 }
-                
+
                 else if (distX > -5 && distX < 5)
                 {
                     if (player.position.Y < enemy.position.Y) theta = (float)Math.PI * 3 / 2;
                     else theta = (float)(Math.PI * 5 / 2);
                 }
-                
                 else
                 {
-                    theta = (float)(Math.Atan(distY / distX) + (2 * Math.PI));
-                    
+                    theta = (float)(Math.Atan(distY / distX) + (2 * Math.PI));   
                     if (player.position.X < enemy.position.X)
                     {
                         if (player.position.Y < enemy.position.Y) theta -= (float)Math.PI; 
@@ -177,54 +218,74 @@ namespace Pong
                 
                 enemy.position.X += da;
                 enemy.position.Y += db;
+                foreach (Character otherEnemy in enemies)
+                {
+                    if (otherEnemy.Touches(enemy)) { enemy.moveBothToStopOverlap(otherEnemy); } 
+                }
             }
         }
         private void MoveBullets(List<Character> bulletList)
         {
+            foreach (Character bullet in bulletList)
+            {
+                // Collision Check
+                foreach (Character enemy in enemyList)
+                {
+                    if ((bullet.position.X < enemy.position.X + bullet.velocity + 12) &&
+                        (bullet.position.X > enemy.position.X - bullet.velocity - 12) && 
+                        (bullet.position.Y < enemy.position.Y + bullet.velocity + 12) &&
+                        (bullet.position.Y > enemy.position.Y - bullet.velocity - 12))
+                    {
+                        enemy.health -= 1;
+                        bullet.health -= 1;
+                    }
+                }
+                // Movement Check
+                if (bullet.health > 0)
+                {
+                    float dx = 0, dy = 0;
+                    switch (bullet.direction)
+                    {
+                        case Direction.Right:
+                            dx = bullet.velocity;
+                            break;
+                        case Direction.Left:
+                            dx = -1 * bullet.velocity;
+                            break;
+                        case Direction.Down:
+                            dy = -1 * bullet.velocity;
+                            break;
+                        case Direction.Up:
+                            dy = bullet.velocity;
+                            break;
+                        case Direction.UpLeft:
+                            dx = -1 * bullet.velocity;
+                            dy = bullet.velocity;
+                            break;
+                        case Direction.UpRight:
+                            dx = bullet.velocity;
+                            dy = bullet.velocity;
+                            break;
+                        case Direction.DownRight:
+                            dy = -1 * bullet.velocity;
+                            dx = bullet.velocity;
+                            break;
+                        case Direction.DownLeft:
+                            dy = -1 * bullet.velocity;
+                            dx = -1 * bullet.velocity;
+                            break;
+                    }
+                    float x = bullet.position.X + dx;
+                    float y = bullet.position.Y - dy;
+                    bullet.position = new Vector2(x, y);
+                }
+            }
             bulletList.RemoveAll(bullet =>
                     (bullet.position.X > _graphics.PreferredBackBufferWidth) ||
                     (bullet.position.X < 0) ||
                     (bullet.position.Y > _graphics.PreferredBackBufferHeight) ||
                     (bullet.position.Y < 0));
-            foreach (Character bullet in bulletList)
-            {
-                float dx = 0, dy = 0;
-                switch (bullet.direction)
-                {
-                    case Direction.Right:
-                        dx = bullet.velocity;
-                        break;
-                    case Direction.Left:
-                        dx = -1 * bullet.velocity;
-                        break;
-                    case Direction.Down:
-                        dy = -1 * bullet.velocity;
-                        break;
-                    case Direction.Up:
-                        dy = bullet.velocity;
-                        break;
-                    case Direction.UpLeft:
-                        dx = -1 * bullet.velocity;
-                        dy = bullet.velocity;
-                        break;
-                    case Direction.UpRight:
-                        dx = bullet.velocity;
-                        dy = bullet.velocity;
-                        break;
-                    case Direction.DownRight:
-                        dy = -1 * bullet.velocity;
-                        dx = bullet.velocity;
-                        break;
-                    case Direction.DownLeft:
-                        dy = -1 * bullet.velocity;
-                        dx = -1 * bullet.velocity;
-                        break;
-                }
-                float x = bullet.position.X + dx;
-                float y = bullet.position.Y - dy;
-                bullet.position = new Vector2(x, y);
-            } 
-           
+            bulletList.RemoveAll(bullet => bullet.health <= 0);
         }
 
         protected override void Update(GameTime gameTime)
@@ -235,91 +296,53 @@ namespace Pong
             var kstate = Keyboard.GetState();
 
             // Event Readers
-            if (kstate.IsKeyDown(Keys.A))
-            {
-                player.position.X -= player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-
-            if (kstate.IsKeyDown(Keys.D))
-            {
-                player.position.X += player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-
-            if (kstate.IsKeyDown(Keys.W))
-            {
-                player.position.Y -= player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-
-            if (kstate.IsKeyDown(Keys.S))
-            {
-                player.position.Y += player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-            if (kstate.IsKeyDown(Keys.Left))
-            {
-                player.direction = Direction.Left;
-            }
-            if (kstate.IsKeyDown(Keys.Right))
-            {
-                player.direction = Direction.Right;
-            }
+            if (kstate.IsKeyDown(Keys.A)) player.position.X -= player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.D)) player.position.X += player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.W)) player.position.Y -= player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.S)) player.position.Y += player.velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Left)) player.direction = Direction.Left;
+            if (kstate.IsKeyDown(Keys.Right)) player.direction = Direction.Right;
+            
             if (kstate.IsKeyDown(Keys.Up))
             {
-                if (kstate.IsKeyDown(Keys.Left))
-                {
-                    player.direction = Direction.UpLeft;
-                }
-                else if (kstate.IsKeyDown(Keys.Right))
-                {
-                    player.direction = Direction.UpRight;
-                }
+                if (kstate.IsKeyDown(Keys.Left)) player.direction = Direction.UpLeft;
+                else 
+                if (kstate.IsKeyDown(Keys.Right)) player.direction = Direction.UpRight;
                 else player.direction = Direction.Up;
             }
             if (kstate.IsKeyDown(Keys.Down))
             {
-                if (kstate.IsKeyDown(Keys.Left))
-                {
-                    player.direction = Direction.DownLeft;
-                }
-                else if (kstate.IsKeyDown(Keys.Right))
-                {
-                    player.direction = Direction.DownRight;
-                }
+                if (kstate.IsKeyDown(Keys.Left)) player.direction = Direction.DownLeft;
+                else 
+                if (kstate.IsKeyDown(Keys.Right)) player.direction = Direction.DownRight;
                 else player.direction = Direction.Down;
             }
 
             // Player Bound Managing
-            if (player.position.X < 15)
-            {
-                player.position.X = 15;
-            }
+            if (player.position.X < 15) player.position.X = 15;
             else if (player.position.X > _graphics.PreferredBackBufferWidth - 15)
             {
                 player.position.X = _graphics.PreferredBackBufferWidth - 15;
             }
-
-            if (player.position.Y < 15)
-            {
-                player.position.Y = 15;
-            }
+            if (player.position.Y < 15) player.position.Y = 15;
             else if (player.position.Y > _graphics.PreferredBackBufferHeight -15)
             {
                 player.position.Y = _graphics.PreferredBackBufferHeight - 15;
             }
 
             // Bullet Time Management
-            if (bulletCoolDown == 0)
+            if (bulletTimer == 0)
             {
                 MakeBullet(player.position, player.direction);
-                bulletCoolDown = 50;
+                bulletTimer = _bulletCD;
             }
-            else bulletCoolDown--;
+            else bulletTimer--;
             MoveBullets(bulletList);
-            
 
+            MoveEnemies(enemyList);
+            // DON'T DELETE
             base.Update(gameTime);
         }
-
-        
 
         protected override void Draw(GameTime gameTime)
         {
@@ -349,7 +372,20 @@ namespace Pong
                     new Vector2(5f, 5f),
                     SpriteEffects.None,
                     0f);
-            } 
+            }
+            foreach(Character enemy in enemyList)
+            {
+                _spriteBatch.Draw(
+                    enemy.texture,
+                    enemy.position,
+                    null,
+                    Color.Red,
+                    0f,
+                    Vector2.Zero,
+                    new Vector2(25f, 25f),
+                    SpriteEffects.None,
+                    0f);
+            }
 
             _spriteBatch.End();
 
